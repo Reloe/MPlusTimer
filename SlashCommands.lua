6401e6f1d3a3dcac822607ca546369b39e0d07f4
Reloe <Jerome.Bellefroid@gmail.com>
2025-10-09 01:52:51 +0200

    add slash command for debug mode and UI

--- SlashCommands.lua
+++ SlashCommands.lua
@@ -1,1 +1,20 @@
 local _, MPT = ...
+
+SLASH_MPT1 = "/mpt"
+SLASH_MPT2 = "/mythicplus"
+SLASH_MPT3 = "/mythicplustimer"
+SlashCmdList["MPT"] = function(msg)
+    if msg == "debug" then
+        if MPTSV.debug then
+            MPTSV.debug = false
+            MPTAPI = {}
+            print("Disabled debug mode for Mythic Plus Timer")
+        else
+            MPTSV.debug = true
+            MPTAPI = MPT
+            print("Enabled debug mode for Mythic Plus Timer, which allows accessing all local functions")
+        end
+    else
+        MPT:ToggleOptions()
+    end
+end
